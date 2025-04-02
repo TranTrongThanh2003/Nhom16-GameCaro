e9fae572b946097eec161d57729356ae35b4cde8
TranThanh <tranthanh2003nt@gmail.com>
2025-04-02 21:54:34 +0700

    cac tep cau hinh va tao giao dien cua so cho server và client

---
+++ FinalClient/src/finalclient/FinalClient.java
@@ -0,0 +1,52 @@
+package finalclient;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class FinalClient {   
+    
+    public static JFrame f;
+    MenuBar menubar;
+    
+    public FinalClient() {
+        // Tạo cửa sổ chính
+        f = new JFrame();
+        f.setTitle("Game Caro");
+        f.setSize(750, 500);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setResizable(false);
+        
+        // Tạo menubar
+        menubar = new MenuBar();
+        
+        // Tạo menu Game
+        Menu game = new Menu("Game");
+        MenuItem newItem = new MenuItem("New Game");
+        MenuItem exit = new MenuItem("Exit");
+        game.add(newItem);
+        game.addSeparator();
+        game.add(exit);
+        
+        // Tạo menu Help
+        Menu help = new Menu("Help");
+        MenuItem helpItem = new MenuItem("Help");
+        MenuItem about = new MenuItem("About");
+        help.add(helpItem);
+        help.add(about);
+        
+        // Thêm menu vào menubar
+        menubar.add(game);
+        menubar.add(help);
+        
+        // Gán menubar vào frame
+        f.setMenuBar(menubar);
+        
+        // Hiển thị cửa sổ
+        f.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new FinalClient();
+    }
+}
